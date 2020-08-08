46a6b27b339eb13017ad7fe8fdb183c143a76d89
root <root@5dec17be5f65>
2020-08-08 00:00:35 +0000

    pi aloud

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,24 +5,29 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-String variable = "250178";
+String variable = "2158";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
 		System.out.println( variable.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+	System.out.println(variable.length());
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
-		
+for(int i=0 ; i<2 ; i++)
+{
+	System.out.println (variable.charAt(0));
+	System.out.println (variable.charAt(1));
+	System.out.println (variable.charAt(2));
+	System.out.println (variable.charAt(3));
+}
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		System.out.println(2);
 	}
 }
 
--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -3,26 +3,35 @@ package _03_char._3_pi_aloud;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.SpecialAction;
+
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
-	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
+	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to    get this.
+String pi = "3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899 8628034825 3421170679";
 	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 	// Check the console shows 3.1
-
+System.out.println (pi.charAt(0));
+System.out.println (pi.charAt(1));
+System.out.println (pi.charAt(2));
 	// 4. Print ALL the digits of of the Pi String (hint: use a loop)
-
+for(int i=0; i<20; i++) {
+	
+}
 	// 5. Use the speak() method below to speak each digit of Pi.
-
+//Speak(pi);
 	// [CHALLENGE]
-	// *6. Get a character from the user using the getInputFromUser() method
+	// *6. Get a character from the user using  the getInputFromUser() method
+getInputFromUser();
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" 
 	
-	
-	
+}
+	 
 	/********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 
 	static void speak(char characterToSpeak) {
